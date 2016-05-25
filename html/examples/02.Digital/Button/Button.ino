8e26efa495f075dd291b4008df7eaf31982ca849
bunnie <bunnie@bunniefoo.com>
2016-05-25 20:08:19 +0800

    button example works!

--- html/examples/02.Digital/Button/Button.ino
+++ html/examples/02.Digital/Button/Button.ino
@@ -1,24 +1,23 @@
+//  This example is Chibitronics Love to Code tested!
 /*
   Button
  
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
+ Turns on and off a light emitting diode(LED) connected to
+ pin A2, when pressing a pushbutton attached to pin A3. 
  
  
  The circuit:
- * LED attached from pin 13 to ground 
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
+ * LED attached from pin A2 to ground
+ * pushbutton attached to pin A3 from ground
+ * on chibitronics boards, A3 can be configured to have an intenal pull-up
  
  created 2005
  by DojoDave <http://www.0j0.org>
  modified 30 Aug 2011
  by Tom Igoe
- 
+ modified 25 May 2016
+ by chibitronics
+
  This example code is in the public domain.
  
  http://www.arduino.cc/en/Tutorial/Button
@@ -26,8 +25,8 @@
 
 // constants won't change. They're used here to 
 // set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int buttonPin = A3;     // the number of the pushbutton pin
+const int ledPin =  A2;      // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
@@ -36,7 +35,7 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);     
+  pinMode(buttonPin, INPUT_PULLUP);     
 }
 
 void loop(){
@@ -44,8 +43,8 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {     
+  // if it is, the buttonState is LOW:
+  if (buttonState == LOW) {     
     // turn LED on:    
     digitalWrite(ledPin, HIGH);  
   } 
