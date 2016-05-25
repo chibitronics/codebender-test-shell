e191cd8db7bfdf3a74f1edfbead639db4c4ed99f
bunnie <bunnie@bunniefoo.com>
2016-05-25 21:49:59 +0800

    analoginoutserial working!

--- html/examples/03.Analog/AnalogInOutSerial/AnalogInOutSerial.ino
+++ html/examples/03.Analog/AnalogInOutSerial/AnalogInOutSerial.ino
@@ -1,3 +1,4 @@
+//  This example is Chibitronics Love to Code tested!
 /*
   Analog input, analog output, serial output
  
@@ -6,14 +7,16 @@
  Also prints the results to the serial monitor.
  
  The circuit:
- * potentiometer connected to analog pin 0.
+ * potentiometer connected to pin A1.
    Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
+   side pins of the potentiometer go to A0 and A3
+ * LED connected from digital pin A2 to ground
  
  created 29 Dec. 2008
  modified 9 Apr 2012
  by Tom Igoe
+ modified 25 May 2016
+ by bunnie
  
  This example code is in the public domain.
  
@@ -21,15 +24,25 @@
 
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9; // Analog output pin that the LED is attached to
+const int analogInPin = A1;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = A2; // Analog output pin that the LED is attached to
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600); 
+  Serial.begin(115200); 
+
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, LOW);   // A0 is the - reference
+  
+  pinMode(A3, OUTPUT);
+  digitalWrite(A3, HIGH);  // A3 is the + voltage
+  
+  pinMode(A1, INPUT);      // A1 is the sensor input
+
+  pinMode(A2, OUTPUT);     // A2 has the LED
+
 }
 
 void loop() {
