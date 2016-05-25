e2784525052d0ce1334cc82100178ee730fed39b
bunnie <bunnie@bunniefoo.com>
2016-05-25 19:32:38 +0800

    updated and modified AnalogReadSerial example

--- html/examples/01.Basics/AnalogReadSerial/AnalogReadSerial.ino
+++ html/examples/01.Basics/AnalogReadSerial/AnalogReadSerial.ino
@@ -1,22 +1,34 @@
 /*
   AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+  Reads an analog input on pin 1, prints the result to the serial monitor.
+
+  Attach the center pin of a potentiometer to pin A1, 
+  and the outside pins to A0 (set to gnd) and A2 (set to VMCU).
  
  This example code is in the public domain.
  */
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // the chibitronics implementation always runs at 115200 regardless of the argument
+  Serial.begin(115200);
+
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, LOW);   // A0 is the - reference
+  
+  pinMode(A2, OUTPUT);
+  digitalWrite(A2, HIGH);  // A2 is the + voltage
+  
+  pinMode(A1, INPUT);      // A1 is the sensor input
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
+  // read the input on analog pin 1:
+  int sensorValue = analogRead(A1);
+
   // print out the value you read:
   Serial.println(sensorValue);
+
   delay(1);        // delay in between reads for stability
 }
