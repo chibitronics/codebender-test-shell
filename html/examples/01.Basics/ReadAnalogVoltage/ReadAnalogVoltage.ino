aeafaeef421b0b0d074e1e4cc8aa00a25cd34d7d
bunnie <bunnie@bunniefoo.com>
2016-05-25 20:51:39 +0800

    readanalogvoltage works!

--- html/examples/01.Basics/ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ html/examples/01.Basics/ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -1,3 +1,4 @@
+//  This example is Chibitronics Love to Code tested!
 /*
   ReadAnalogVoltage
   Reads an analog input on pin A1, converts it to voltage, and prints the result to the serial monitor.
@@ -7,7 +8,7 @@
  */
 
 // the setup routine runs once when you press reset:
-int calibrationValue;
+float calibrationValue;
 void setup() {
   // the chibitronics implementation always runs at 115200 regardless of the argument
   Serial.begin(115200);
@@ -23,7 +24,8 @@ void setup() {
   pinMode(A6, INPUT);      // A6 is the "bandgap" reference, it is stable at 1V over temperature and different chips
 
   // use A6 bandgap value to calibrate our voltage!
-  calibrationValue = analogRead(A6);
+  calibrationValue = (float) analogRead(A6);  // calibrationValue is the digital code equivalent to 1.0V!
+  // just divide by calibtrationValue to get the actual voltage in volts
 }
 
 // the loop routine runs over and over again forever:
@@ -31,7 +33,7 @@ void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A1);
   // Convert the analog reading (which goes from 0 - 4096) to a voltage (0 - 3.3V):
-  float voltage = (sensorValue / calibrationValue) * 1.0;
+  float voltage = (sensorValue / calibrationValue);
   // print out the value you read:
   Serial.println(voltage);
 }
