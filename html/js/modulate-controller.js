e1a6d9db09eca23989bda1e342124985f820a795
Sean Cross <sean@xobs.io>
2016-05-31 16:16:04 +0800

    modulate-controller: simplify packet generation
    Instead of working with buffer offsets, construct the packet and
    work on individual elements.
    
    Bring common components out to functions.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- html/js/modulate-controller.js
+++ html/js/modulate-controller.js
@@ -20,6 +20,11 @@
         this.byteArray = null;
         this.playCount = 0;
 
+        this.PROT_VERSION = 0x01;   // Protocol v1.0
+
+        this.CONTROL_PACKET = 0x01;
+        this.DATA_PACKET = 0x02;
+
         this.modData = new modulator(this); // the modulator object contains our window's audio context
 
         /* Preamble sent before every audio packet */
@@ -27,10 +32,19 @@
     }
 
     ModulationController.prototype = {
+
+        makeControlHeader: function() {
+            return [this.PROT_VERSION, this.CONTROL_PACKET, 0x00, 0x00];
+        },
+
+        makeDataHeader: function(blockNum) {
+            return [this.PROT_VERSION, this.DATA_PACKET, blockNum & 0xff, (blockNum >> 8) & 0xff];
+        },
+
         sendTestData: function() {
             // assemble a single test data packet
             var preamble = this.preamble;
-            var sector = [0x01, 0x02, 0x80, 0x04]; // version code + two bytes for sector offset
+            var sector = this.makeDataHeader(0x03); // version code + two bytes for sector offset
             // note to self: version codes are now checked by Rx so let's not mess with that anymore
             // 256 byte payload, preamble, sector offset + 4 bytes hash + 1 byte stop
             var packetlen = 256 + preamble.length + sector.length + 4 + 1;
@@ -84,7 +98,7 @@
         sendTestCtrl: function() {
             // assemble a single control packet for testing
             var preamble = this.preamble;
-            var sector = [0x01, 0x01, 0x00, 0x00];   // version 1, packet type 1(ctrl), two bytes of padding
+            var sector = this.makeControlHeader();
             var packetlen = 24 + preamble.length + sector.length + 4 + 1;
             var buffer = new Uint8Array(packetlen);
             var i = 0;
@@ -179,101 +193,109 @@
             }
         },
 
+        makeUint32: function(num) {
+            return [num & 0xff,
+                   (num >> 8) & 0xff,
+                   (num >> 16) & 0xff,
+                   (num >> 24) & 0xff];
+        },
+
+        makeUint16: function(num) {
+            return [num & 0xff,
+                   (num >> 8) & 0xff];
+        },
+
+        makePacket: function() {
+            var len = 0;
+            var i;
+            for (i = 0; i < arguments.length; i++)
+                len += arguments[i].length;
+
+            var pkt = new Uint8Array(len);
+            var offset = 0;
+
+            for (i = 0; i < arguments.length; i++)
+                offset += this.appendData(pkt, arguments[i], offset);
+
+            return pkt;
+        },
+
         makeCtlPacket: function(data) {
             // parameters from microcontroller spec. Probably a better way
             // to do this in javascript, but I don't know how (seems like "const" could be used, but not universal)
-            var bytePreamble = this.preamble;
-            var byteVersion = [0x01];
-            var byteType = [0x01];
-            var bytePadding = [0x00, 0x00];
-            var pktLength = data.length;
-            var byteLength = [pktLength & 0xFF, (pktLength >> 8) & 0xFF,
-                              (pktLength >> 16) & 0xFF, (pktLength >> 24) & 0xFF];
-            var pktFullhash = murmurhash3_32_gc(data, 0x32d0babe);  // 0x32d0babe by convention
-            var guidStr = SparkMD5.hash(String.fromCharCode.apply(null,data), false);
-            var pktGuid = [];
+            var preamble = this.preamble;
+            var header = this.makeControlHeader();
+            var program_length = this.makeUint32(data.length);
+            var program_hash = this.makeHash(data, 0x32d0babe);  // 0x32d0babe by convention
+            var program_guid_str = SparkMD5.hash(String.fromCharCode.apply(null,data), false);
+            var program_guid = [];
             var i;
-            for (i = 0; i < guidStr.length-1; i += 2)
-                pktGuid.push(parseInt(guidStr.substr(i,2),16));
+            for (i = 0; i < program_guid_str.length-1; i += 2)
+                program_guid.push(parseInt(program_guid_str.substr(i,2),16));
 
-            var packetlen = bytePreamble.length + bytePadding.length + byteVersion.length + byteType.length + byteLength.length + 4 + pktGuid.length + 4 + 1;
-            var pkt = new Uint8Array(packetlen);
-            var pktIndex = 0;
-            for (i = 0; i < bytePreamble.length; i++)
-                pkt[pktIndex++] = bytePreamble[i];
+            var footer = this.makeFooter(header, program_length, program_hash, program_guid);
+            var stop = [0xff];
 
-            pkt[pktIndex++] = byteVersion[0];
-            pkt[pktIndex++] = byteType[0];
-            pkt[pktIndex++] = bytePadding[0];
-            pkt[pktIndex++] = bytePadding[1];
-            for (i = 0; i < byteLength.length; i++)
-                pkt[pktIndex++] = byteLength[i];
+            return this.makePacket(preamble, header, program_length, program_hash, program_guid, footer, stop);
+        },
 
-            pkt[pktIndex++] = pktFullhash & 0xFF;
-            pkt[pktIndex++] = (pktFullhash >> 8) & 0xFF;
-            pkt[pktIndex++] = (pktFullhash >> 16) & 0xFF;
-            pkt[pktIndex++] = (pktFullhash >> 24) & 0xFF;
-            for (i = 0; i < 16; i++)
-                pkt[pktIndex++] = pktGuid[i];
+        /* Appends "src" to "dst", beginning at offset "offset".
+         * Handy for populating data buffers.
+         */
+        appendData: function(dst, src, offset) {
+            var i;
+            for (i = 0; i < src.length; i++)
+                dst[offset + i] = src[i];
+            return i;
+        },
 
-            var hash = murmurhash3_32_gc(pkt.subarray(bytePreamble.length, 24 + bytePreamble.length + byteVersion.length + byteType.length + bytePadding.length), 0xdeadbeef); // deadbeef is just by convention
-            pkt[pktIndex++] = hash & 0xFF;
-            pkt[pktIndex++] = (hash >> 8) & 0xFF;
-            pkt[pktIndex++] = (hash >> 16) & 0xFF;
-            pkt[pktIndex++] = (hash >> 24) & 0xFF
-            pkt[pktIndex] = 0xFF;  // terminate with 0xFF to let last bit demodulate correctly
+        makeHash: function(data, hash) {
+            return this.makeUint32(murmurhash3_32_gc(data, hash));
+        },
 
-            return pkt;
+        makeFooter: function(packet) {
+            var hash = 0xdeadbeef;
+            var data = new Array();
+            var i;
+            var j;
+
+            // Join all argument arrays together into "data"
+            for (i = 0; i < arguments.length; i++)
+                for (j = 0; j < arguments[i].length; j++)
+                    data.push(arguments[i][j]);
+
+            return this.makeHash(data, hash);
         },
 
         makeDataPacket: function(dataIn, blocknum) {
             var data;
             var i;
-            if (dataIn.length != 256) {
-                // if our data array isn't a whole packet in length, pad it out with FF's
-                data = new Uint8Array(256);
-                for( i = 0; i < dataIn.length; i ++ ) {
-                    data[i] = dataIn[i];
-                }
-                for( ; i < 256; i++ ) {
-                    data[i] = 0xFF; // 1's pad out the final data packet
-                }
-            }
-            else {
-                data = dataIn;
-            }
 
             // now assemble the packet
             var preamble = this.preamble;
-            var sector = [0x01, 0x02, blocknum & 0xFF, (blocknum >> 8) & 0xFF];   // version 1
+            var header = this.makeDataHeader(blocknum);
+
+            // Ensure the "data" payload is 256 bytes long.
+            var data = new Uint8Array(256);
+            data.fill(0xff);    // Pad extra data with 0xff
+            this.appendData(data, dataIn, 0);
+
+            var footer = this.makeFooter(header, data);
+            var stop = [0xff];
+
             // 256 byte payload, preamble, sector offset + 4 bytes hash + 1 byte stop
-            var packetlen = 256 + preamble.length + sector.length + 4 + 1;
-
-            var buffer = new Uint8Array(packetlen);
-            for (i = 0; i < preamble.length; i++)
-                buffer[i] = preamble[i];
-            for (var j = 0; i < sector.length + preamble.length; i++, j++)
-                buffer[i] = sector[j];
-            for (j = 0; i < packetlen - 1 - 4; i++, j++)
-                buffer[i] = data[j];
-
-            var hash = murmurhash3_32_gc(buffer.subarray(preamble.length, 256 + preamble.length + sector.length), 0xdeadbeef);
-            buffer[i++] = hash & 0xFF;
-            buffer[i++] = (hash >> 8) & 0xFF;
-            buffer[i++] = (hash >> 16) & 0xFF;
-            buffer[i++] = (hash >> 24) & 0xFF
-            buffer[i] = 0xFF;  // terminate with 0xFF to let last bit demodulate correctly
+            var packetlen = preamble.length + header.length + data.length + footer.length + stop.length;
 
             // now stripe the buffer to ensure transitions for baud sync
             // don't stripe the premable or the hash
-            for (i = 9; i < (buffer.length - 5); i++) {
-                if ((i % 16) == 14)
-                    buffer[i] ^= 0x55;
-                else if ((i % 16) == 6)
-                    buffer[i] ^= 0xaa;
+            for (i = 0; i < data.length; i++) {
+                if ((i % 16) == 3)
+                    data[i] ^= 0x55;
+                else if ((i % 16) == 11)
+                    data[i] ^= 0xaa;
             }
 
-            return buffer;
+            return this.makePacket(preamble, header, data, footer, stop);
         },
 
         // once all audio is done playing, call this to reset UI elements to idle state
