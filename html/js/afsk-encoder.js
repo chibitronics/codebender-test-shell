351d7c185f7e45a723809816f7c2aef0c19effc6
Sean Cross <sean@xobs.io>
2016-06-01 13:41:59 +0800

    afsk-encoder: replace tab with eight spaces
    This makes the code line up properly.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- html/js/afsk-encoder.js
+++ html/js/afsk-encoder.js
@@ -13,25 +13,19 @@ FskEncoder.prototype = {
     f_lo: 8666,
     f_hi: 12500,
     baud_rate: 8000,
-//    f_lo: 8674,
-//    f_hi: 12512,
-//    baud_rate: 8008,
-//    f_lo: 8662,
-//    f_hi: 12498,
-//    baud_rate: 7996,
     sample_rate: 0, // comes from calling function based on browser/computer config
     samplesPerBit: 0.0,
 
     state : {
-	phase: 0,
-	omega_lo: 0,
-	omega_hi: 0,
-	current_bit: 0,
-	baud_frac: 0,
-	baud_incr: 0,
-	curbyte: 0,
-	bitpos: 0,
-	datapos: 0,
+        phase: 0,
+        omega_lo: 0,
+        omega_hi: 0,
+        current_bit: 0,
+        baud_frac: 0,
+        baud_incr: 0,
+        curbyte: 0,
+        bitpos: 0,
+        datapos: 0,
     },
 
     PHASE_BITS: 16,
@@ -39,45 +33,44 @@ FskEncoder.prototype = {
 
     // compute samples per bit. Needed to allocate audio buffers before modulating
     samplesPerBit: function() {
-	return  this.sample_rate / this.baud_rate; // Not rounded! Floating point!
+        return  this.sample_rate / this.baud_rate; // Not rounded! Floating point!
     },
 
     // for debug.
     dumpBuffer: function(buf) {
-	var out = "";
-	for (var i = 0; i < buf.length; i++)
-	    out += "0x" + buf[i].toString(16) + ",";
-	return out;
+        var out = "";
+        for (var i = 0; i < buf.length; i++)
+            out += "0x" + buf[i].toString(16) + ",";
+        return out;
     },
 
     // does what you think it does -- input data should be uint8 array, outputdata is floats
     modulate: function(inputData, outputData) {
-	for(var i = 0; i < outputData.length; i++) {
-	    this.state.baud_frac += this.state.baud_incr;
-	    if( this.state.baud_frac >= 1) {
-		this.state.baud_frac -= 1;
-		if( this.state.bitpos == 0 ) {
-		    if( this.state.datapos <= inputData.length ) {
-			this.state.curbyte = inputData[this.state.datapos++];
-			this.state.bitpos = 8;
-		    } else {
-			return outputData;
-		    }
-		}
-		this.state.current_bit = this.state.curbyte & 1;
-		this.state.curbyte >>= 1;
-		this.state.bitpos--;
-	    }
-	    outputData[i] = Math.cos(this.state.phase);
-	    if( this.state.current_bit == 0) {
-		this.state.phase += this.state.omega_lo;
-	    } else {
-		this.state.phase += this.state.omega_hi;
-	    }
-	}
+        for(var i = 0; i < outputData.length; i++) {
+            this.state.baud_frac += this.state.baud_incr;
+            if( this.state.baud_frac >= 1) {
+                this.state.baud_frac -= 1;
+                if( this.state.bitpos == 0 ) {
+                    if( this.state.datapos <= inputData.length ) {
+                        this.state.curbyte = inputData[this.state.datapos++];
+                        this.state.bitpos = 8;
+                    } else {
+                        return outputData;
+                    }
+                }
+                this.state.current_bit = this.state.curbyte & 1;
+                this.state.curbyte >>= 1;
+                this.state.bitpos--;
+            }
+            outputData[i] = Math.cos(this.state.phase);
+            if( this.state.current_bit == 0) {
+                this.state.phase += this.state.omega_lo;
+            } else {
+                this.state.phase += this.state.omega_hi;
+            }
+        }
 
-	this.state.datapos = 0;
-	return outputData;
+        this.state.datapos = 0;
+        return outputData;
     },
 }
-
