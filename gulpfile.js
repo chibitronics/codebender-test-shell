4601b9797dadc7fe57e3d275e89ed2c525cab5ff
Sean Cross <sean@xobs.io>
2016-06-02 14:40:22 +0800

    gulpfile: add our curent build file
    This provides a base for building everything.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', function() {
+        console.log("Hello, world!");
+});
+
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')           /* Load all HTML files */
+        .pipe(useref())                     /* Combine files into one */
+        .pipe(gulpIf('*.js', uglify()))     /* minify javascript files */
+        .pipe(gulpIf('*.css', cssnano()))   /* minify css files */
+        .pipe(gulp.dest('html'))            /* Write out to 'html' output directory */
+});
+
+gulp.task('cpjson', function() {
+    return gulp.src('app/*.json')
+        .pipe(gulp.dest('html'))
+});
+
+gulp.task('cpimages', function() {
+    return gulp.src('app/images/**/*.+(png|gif|jpg|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('html/images'))
+});
+
+gulp.task('clean:html', function() {
+    return del.sync('html');
+});
+
+gulp.task('cache:clear', function (callback) {
+    return cache.clearAll(callback);
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+});
+
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', function(callback) {
+    runSequence('clean:html',
+                ['useref', 'cpimages', 'cpjson'],
+                callback
+    );
+});
