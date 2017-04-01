c0076089df8fef5eafd68b978b05d8b0b39299d4
Sean Cross <sean@xobs.io>
2017-04-01 20:21:50 +0800

    chibi-lint: make jshint happy
    Refactor the linter to make jshint happy.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/js/chibi-lint.js
+++ src/js/chibi-lint.js
@@ -2,87 +2,85 @@
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
 (function (mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../node_modules/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../node_modules/codemirror"], mod);
-  else // Plain browser env
+  if (typeof exports === 'object' && typeof module === 'object') { // CommonJS
+    mod(require('../../node_modules/codemirror'));
+  }
+  else if (typeof define === 'function' && define.amd) { // AMD
+    define(['../../node_modules/codemirror'], mod);
+  }
+  else {// Plain browser env
     mod(CodeMirror);
+  }
 })(function (CodeMirror) {
-  "use strict";
+  'use strict';
 
-  CodeMirror.registerHelper("lint", "text/x-c++src", function (text, passOptions, cm) {
+  CodeMirror.registerHelper('lint', 'text/x-c++src', function (text, passOptions, cm) {
     var found = [];
-    var internal_error_str = "";
+    var internalErrorString = '';
 
-    var error_message = cm.chibi_error_string;
-    if (!error_message) {
-      cm.last_internal_error_str = internal_error_str;
+    var errorMessage = cm.chibiErrorString;
+    if (!errorMessage) {
+      cm.lastInternalErrorString = internalErrorString;
       return found;
     }
 
-    var lines = error_message.split("\n");
+    var lines = errorMessage.split('\n');
     var i;
 
-    console.log("Error message: " + error_message);
-    // The number of "unknown error" lines we can skip.
+    console.log('Error message: ' + errorMessage);
+    // The number of 'unknown error' lines we can skip.
     // We set this because error messages tend to give two lines of context,
     // which we can ignore.
-    var skip_lines = 0;
+    var skipLines = 0;
     for (i = 0; i < lines.length; i++) {
       var line = lines[i];
-      var inofilestr = "(sketch file) ";
-      var exitstatusstr = "exit status ";
-      var compileterm = "compilation terminated";
+      var inofilestr = '(sketch file) ';
+      var exitstatusstr = 'exit status ';
+      var compileterm = 'compilation terminated';
       if (line.substr(0, inofilestr.length) === inofilestr) {
-        var parts = line.split(":");
-        var filename = parts.shift();
+        var parts = line.split(':');
+        /*var filename =*/ parts.shift();
         var lineno = parts.shift() - 1;
-        var error = parts.join(":");
+        var error = parts.join(':');
 
         found.push({ from: CodeMirror.Pos(lineno, 0), to: CodeMirror.Pos(lineno, 0), message: error });
-        skip_lines = 2;
+        skipLines = 2;
       }
 
-      // Just ignore the "exit status 1" line
+      // Just ignore the 'exit status 1' line
       else if (line.substr(0, exitstatusstr.length) === exitstatusstr) {
-        ;
       }
 
-      // Also ignore the "compilation terminated" line
+      // Also ignore the 'compilation terminated' line
       else if (line.substr(0, compileterm.length) === compileterm) {
-        ;
       }
 
-      // Ignore "changing start of text section" messages.
-      else if (line.indexOf("warning: changing start of section .text by 4 bytes") > -1) {
-        ;
+      // Ignore 'changing start of text section' messages.
+      else if (line.indexOf('warning: changing start of section .text by 4 bytes') > -1) {
       }
 
-      // Ignore "[filename]: In function [function]" messages, since they're followed by the error
-      else if (line.indexOf(": In function ") > -1) {
-        ;
+      // Ignore '[filename]: In function [function]' messages, since they're followed by the error
+      else if (line.indexOf(': In function ') > -1) {
       }
 
       // Ignore blank lines.
       else if (!line) {
-        ;
       }
 
       // If we have lines we can skip, ignore those too.
-      else if (skip_lines > 0) {
-        skip_lines--;
+      else if (skipLines > 0) {
+        skipLines--;
       }
 
       else {
-        internal_error_str += "\n" + line;
+        internalErrorString += '\n' + line;
       }
     }
 
-    if ((internal_error_str !== "") && (internal_error_str !== cm.last_internal_error_str)) {
-      document.getElementById("internal_error_text").innerHTML = internal_error_str;
-      //document.getElementById("internal_error_dialog").dialog("open");
-      cm.last_internal_error_str = internal_error_str;
+    if ((internalErrorString !== '') && (internalErrorString !== cm.lastInternalErrorString)) {
+      document.getElementById('internal_error_text').innerHTML = internalErrorString;
+      //document.getElementById('internal_error_dialog').dialog('open');
+      cm.lastInternalErrorString = internalErrorString;
     }
     return found;
   });
