5c520c1103e3aca7d8896841e257c2463dbac685
bunnie <bunnie@chibitronics.com>
2017-06-08 19:22:19 +0800

    fix missing void in simple example

--- examples-ltc/6.LTCVolume1/Test1/Test1.ino
+++ examples-ltc/6.LTCVolume1/Test1/Test1.ino
@@ -3,11 +3,11 @@
 // Volume 1 syntax demo
 
 // The simplest possible blink example is shown here using the LTC API.
-setup() {
+void setup() {
   outputMode(0);
 }
 
-loop() {
+void loop() {
   on(0);
   pause(1000);
   off(0);
