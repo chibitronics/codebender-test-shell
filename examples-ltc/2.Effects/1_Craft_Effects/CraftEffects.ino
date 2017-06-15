a113d5661dce0a1dabf8d5683efdb1460fe827fe
Sean Cross <sean@xobs.io>
2017-06-15 16:51:38 +0800

    examples-ltc: crafteffects: remove createThreadFromHeap
    This is now built in, so remove the definition here.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- examples-ltc/2.Effects/1_Craft_Effects/CraftEffects.ino
+++ examples-ltc/2.Effects/1_Craft_Effects/CraftEffects.ino
@@ -126,18 +126,6 @@ static void effects_thread(void *arg) {
   }
 }
 
-static thread_t *createThreadFromHeap(size_t size, tprio_t prio,
-                                      tfunc_t pf, void *arg) {
-  thread_t *thr = (thread_t *)malloc(THD_WORKING_AREA_SIZE(size));
-  createThread((void *)thr, THD_WORKING_AREA_SIZE(size), prio, pf, arg);
-
-  /* Mark thr->p_flags as CH_FLAG_MODE_HEAP, so ChibiOS will call free()
-   * on the memory after it exits.
-   */
-  ((uint8_t *)thr)[0x1d] = 1;
-  return thr;
-}
-
 void setup(void) {
   int i;
 
