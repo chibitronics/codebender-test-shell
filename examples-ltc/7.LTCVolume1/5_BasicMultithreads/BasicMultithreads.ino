7dc0cd683b1fb22a65bbed3fcaf54ff8306a01e9
Jie <jieqi@mit.edu>
2017-06-23 16:47:32 -0400

    updating multithreading example for LTC Vol 1

--- examples-ltc/7.LTCVolume1/5_BasicMultithreads/BasicMultithreads.ino
+++ examples-ltc/7.LTCVolume1/5_BasicMultithreads/BasicMultithreads.ino
@@ -10,7 +10,7 @@
 // The easiest way to use this code is to imagine you are writing a separate program,
 // with differently named variable names and pin names by number, for each output pin on Chibi Chip.
 // 
-// For this code to run, we must have all six setup and loop threads in the code even if 
+// For this code to run, we must have all six setup0 through setup5 and loop0 through loop5 functions in the code even if 
 // they are blank.
 
 #include "ChibiOS.h" // we need to include our special code to enable threading
@@ -26,46 +26,22 @@ void setup0() {
 
 void loop0() {
   // thread 0's loop code here
-  int brightness0 = 0;
-
-  while (brightness0 < 100){
-    setLevel(led0, brightness0);
-    pause(10);
-    brightness0 = brightness0 + 1;
-  }
-  
-  while (brightness0 > 0){
-    setLevel(led0, brightness0);
-    pause(10);
-    brightness0 = brightness0 - 1;
-  }
-                          
+  on(led0);
+  pause(300);
+  off(led0);
+  pause(300);                     
 }
 
 ///// thread 1
-int led1 = 1;
-
 void setup1() {
-  // thread 1’s specific setup code here
-  outputMode(led1);
+  outputMode(1);
 }
 
 void loop1() {
-  // thread 1’s loop code here
-  int brightness1 = 100;
-
-  while (brightness1 > 0){
-    setLevel(led1, brightness1);
-    pause(10);
-    brightness1 = brightness1 - 1;
-  }
-  
-  while (brightness1 < 100){
-    setLevel(led1, brightness1);
-    pause(10);
-    brightness1 = brightness1 + 1;
-  }
-                          
+  on(1);
+  pause(500);
+  off(1);
+  pause(500);                      
 }
 
 
@@ -76,9 +52,9 @@ void setup2() {
 
 void loop2() {
   on(2);
-  pause(500);
+  pause(800);
   off(2);
-  pause(500);
+  pause(800);
 }
 
 ///// thread 3 
@@ -88,32 +64,52 @@ void setup3() {
 
 void loop3() {
   on(3);
-  pause(400);
+  pause(1000);
   off(3);
-  pause(400);
+  pause(1000);
 }
 
 ///// thread 4 
+int led4 = 4;
 void setup4() {
   outputMode(4);
 }
 
 void loop4() {
-  on(4);
-  pause(600);
-  off(4);
-  pause(600);
-}
+  int brightness4 = 0;  
 
+  while(brightness4 < 100) {
+    setLevel(led4, brightness4); 
+    pause(10);                
+    brightness4 = brightness4 + 1;
+  } 
+  
+  
+while(brightness4 > 0) {
+    setLevel(led4, brightness4); 
+    pause(10);                
+    brightness4 = brightness4 - 1;
+  }
+}
 ///// thread 5 
+int led5 = 5;
 void setup5() {
   outputMode(5);
 }
 
 void loop5() {
-  on(5);
-  pause(1000);
-  off(5);
-  pause(1000);
-}
+  int brightness5 = 100;  
 
+  while(brightness5 > 0) {
+    setLevel(led5, brightness5); 
+    pause(10);                
+    brightness5 = brightness5 - 1;
+  } 
+  
+  
+while(brightness5 < 100) {
+    setLevel(led5, brightness5); 
+    pause(10);                
+    brightness5 = brightness5 + 1;
+  }
+}
