c228a7643b97e3ed3440f491a595a7aca00e689d
Jie <jieqi@mit.edu>
2017-07-03 21:31:43 -0400

    adding basic light sensor to LTC vol 1 example code

---
+++ examples-ltc/7.LTCVolume1/6_LightSensor/BasicLightSensor.ino
@@ -0,0 +1,22 @@
+// Love to Code
+// Volume 1: Basic Light Sensor
+
+// This code reads a light sensor on pin 5. 
+// Pin 0 turns ON when it is dark and OFF when it is bright.
+
+int lightLevel = 0;  // make a variable named lightLevel to save current light level
+
+void setup(){
+  outputMode(0);  // LED on pin 0 will turn on and off
+  inputMode(5);   // light sensor is attached to pin 5
+}
+
+void loop(){
+  bright = read(5);  // read current light level
+  
+  if (bright == 1){  
+      off(0);        // if light level is bright, turn off pin 0     
+    } else {
+      on(0);         // otherwise turn on pin 0
+    }
+}
